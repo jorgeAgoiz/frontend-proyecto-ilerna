a9c97bc6bf022bd509f5f14b83cf516640efbcb7
jorgeAP <jorgeagoiz@gmail.com>
2021-10-04 11:34:38 +0200

    Estilando la lista de libros de la comunidad

--- src/components/ListBooks/ListBooks.js
+++ src/components/ListBooks/ListBooks.js
@@ -4,13 +4,36 @@ import './ListBooks.css'
 const ListBooks = ({ allBooks }) => {
   console.log(allBooks)
 
+  const showBooks = (books) => {
+    return books.map(book => {
+      return (
+        <tr className='book-card' key={book.id}>
+          <td>{book.title}</td>
+          <td>{book.author}</td>
+          <td>{book.category}</td>
+        </tr>
+      )
+    })
+  }
+
   return (
     <div className='list-books'>
       <h1>List Of Books</h1>
+      <table className='book-table'>
+        <tr className='book-card'>
+          <th>Titulo</th>
+          <th>Autor</th>
+          <th>Categoría</th>
+        </tr>
+        {
+          showBooks(allBooks.data)
+        }
+      </table>
     </div>
   )
 }
 
 export default ListBooks
 
-/* Aqui ya empezaremos a mostrar el listado de libros */
+/* Aqui ya empezaremos a mostrar el listado de libros,
+necesitamos estilar bien la tabla y mostrar las paginas debajo. */
