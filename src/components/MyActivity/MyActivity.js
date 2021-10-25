76ef00bbc4ce8a0b145618661fdbfc01493c0d78
jorgeAP <jorgeagoiz@gmail.com>
2021-10-25 08:31:44 +0200

    Sección 'Mi actividad' estilada

--- src/components/MyActivity/MyActivity.js
+++ src/components/MyActivity/MyActivity.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GenericBtn from '../GenericBtn/GenericBtn'
 import './MyActivity.css'
 
 const MyActivity = () => {
@@ -11,17 +12,55 @@ const MyActivity = () => {
       <div className='books-main'>
         <h2>Libros</h2>
         <div className='list-of-books'>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+
           {/* Aqui iran los libros */}
         </div>
       </div>
       <div className='review-main'>
         <h2>Reseñas</h2>
         <div className='list-of-reviews'>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+          <p>Hola que tal</p>
+
           {/* Aqui iran las reseñas */}
         </div>
       </div>
-      <div className='delete-user-btn'>
-        <button>Eliminar usuario</button>
+      <div className='delete-user'>
+        <GenericBtn
+          onClickFunc={() => console.log('Hola que tal')}
+          text='Eliminar usuario'
+          nameClass='delete-user-btn'
+        />
       </div>
     </div>
   )
